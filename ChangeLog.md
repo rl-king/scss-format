3f23b712d636f8898422fd15c4238b1e4497f9bd
rl-king <hello@rubenlieking.com>
2020-05-29 19:10:04 +0200

    Format bench, rm changelog, rm stylish conf

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for scss-format
-
-## Unreleased changes
--- bench/Main.hs
+++ bench/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Criterion.Main
@@ -6,12 +7,11 @@ import Data.Text.IO as Text
 import Language.Scss.Format
 import Language.Scss.Parser
 
-
 main :: IO ()
 main = do
   file <- Text.readFile "style.scss"
   let (Right parsed) = parse file
   defaultMain
-    [ bench "parse file" $ whnf parse file
-    , bench "format file" $ whnf format parsed
+    [ bench "parse file" $ whnf parse file,
+      bench "format file" $ whnf format parsed
     ]
